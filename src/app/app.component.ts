647c0977fee9d7a999347ba7d8b75ae2cb4c1386
Mohamed Beshri Amer <mohamed.perry351@gmail.com>
2021-02-13 05:20:54 -0800

    I forget to do commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TestingApp';
+
+  constructor(private ref: ChangeDetectorRef) {
+
+
+
+  }
+  complexObject: any= {
+    firstName: "Mohamed",
+    middleName:"Beshri",
+    lastName: "Amer",
+    products: [
+      { id: 1, name: "pc", price: 5000 },
+      { id: 2, name: "AnyThing", price: 6000 },
+      { id: 3, name: "Hello World", price: 7000 },
+    ],
+    friends: [
+      { id: 1, name: "Sayed Hawas" },
+      { id: 2, name: "Abd- elharman" },
+      { id: 3, name: "Yousef" },
+      { id: 4, name: "Elhussien" },
+      {id:4,name:"Osama"},
+      { id: 5, name: "Ahmed" },
+      { id: 6, name: "Mina" },
+      { id: 7, name: "Abanob" },
+      { id: 6, name: "Shahendia" },
+      { id: 7, name: "Nardeen" },
+      { id: 8, name: "Andaleeb" },
+
+    ]
+
+  };
+
+  CheckAfterChange(event) {
+    console.log(event);
+    alert("Changed");
+  }
+
+
+
 }
